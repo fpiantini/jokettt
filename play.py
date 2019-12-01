50b22896bf2893d8d71d1c0426c2bca2cd2129c1
Francesco Piantini <francesco.piantini@gmail.com>
2019-12-01 18:46:53 +0100

    refactoring of play.py main, removed pylint complaints

--- play.py
+++ play.py
@@ -1,51 +1,90 @@
 #!/usr/bin/env python3
 #
-import tttboard
+"""Play a series of tic-tac-toe games between an human and an AI player"""
+from argparse import ArgumentParser
+
 from tttboard import TttBoard
 from tttconsoleplayer import TttConsolePlayer
 from tttlearnerplayer import TttLearnerPlayer
+from tttminimaxplayer import TttMinimaxPlayer
 
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
-def play_a_game(p1, p2, board):
+def play_human_vs_ai_game(human_player, ai_player, auto_player_first, board):
+    """Play a tic-tac-toe game between an human and an AI player"""
     result = 0
     print("----------------------------------------------------")
     print("  --- NEW GAME ---")
-    p1_turn = True
+    console_player_turn = not auto_player_first
+
+    if console_player_turn:
+        print('%s' % board)
+
     while result == 0 and board.is_not_full():
 
-        print('%s' % board)
-        if p1_turn:
-            x, y = p1.move(board)
-            zhash, result = board.place_pawn(x, y, p1.piece)
+        if console_player_turn:
+            _x, _y = human_player.move(board)
+            _, result = board.place_pawn(_x, _y, human_player.piece)
         else:
-            x, y = p2.move(board)
-            zhash, result = board.place_pawn(x, y, p2.piece)
+            _x, _y = ai_player.move(board)
+            _, result = board.place_pawn(_x, _y, ai_player.piece)
 
-        p1_turn = not p1_turn
+        print('%s' % board)
+        console_player_turn = not console_player_turn
 
     print('%s' % board)
-    if (result > 0):
-        print("You lose! :-D")
-    elif (result < 0):
-        p2.learn_from_defeat(board)
-        print("You win!! :-(")
-    else:
-        print("Draw! ;-)")
+
+    if result > 0:
+        # AI player wins
+        return -1
+    if result < 0:
+        # Human wins
+        if isinstance(ai_player, TttLearnerPlayer):
+            ai_player.learn_from_defeat(board)
+        return 1
+    # draw
+    return 0
 
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
 #   ***  MAIN ***
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
-board = TttBoard('x', 'o')
-lPlayer = TttLearnerPlayer('x', board)
-cPlayer = TttConsolePlayer('o')
-while True:
-    board.reset()
-    play_a_game(cPlayer, lPlayer, board)
+def main():
+    """Main program: parses options, declare board and players, and
+        plays a series of games"""
+    parser = ArgumentParser()
+    parser.add_argument("player_mode", help="the mode of the player",
+                        choices=["minimax", "learner"], nargs='?', default="minimax")
+    parser.add_argument("-s", "--second",
+                        help="give the first move to the machine", action="store_true")
+    args = parser.parse_args()
 
-# --------------------------------------------------------------------
-# --------------------------------------------------------------------
+    if args.second:
+        print("FIRST MOVE TO THE MACHINE!")
+    else:
+        print("FIRST MOVE TO YOU!")
 
+    print("TYPE OF AI PLAYER = {}".format(args.player_mode))
 
+    board = TttBoard('x', 'o')
+    if args.player_mode == "minimax":
+        auto_player = TttMinimaxPlayer('x')
+    else:
+        auto_player = TttLearnerPlayer('x', board)
+
+    console_player = TttConsolePlayer('o')
+
+    while True:
+        board.reset()
+        res = play_human_vs_ai_game(console_player, auto_player, args.second, board)
+
+        if res < 0:
+            print("You lose! :-D")
+        elif res > 0:
+            print("You win!! :-(")
+        else:
+            print("Draw! ;-)")
+
+if __name__ == "__main__":
+    main()
